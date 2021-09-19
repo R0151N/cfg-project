30e2cf4b889e5aa9d8c1f78f1ce74dcaad6a6cd6
R0151N <88348588+R0151N@users.noreply.github.com>
2021-09-19 21:14:58 +0100

    Added Expanding Grid at bottom of home page

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//Expanding Grid
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+
+
 //Filter
 filterSelection("all")
 function filterSelection(c) {
