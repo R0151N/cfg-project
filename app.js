6a93372b97d6604af6e931fdc0aeab640842e1d3
R0151N <88348588+R0151N@users.noreply.github.com>
2021-09-21 14:55:37 +0100

    Scroll to the top button

--- app.js
+++ app.js
@@ -60,3 +60,28 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 //End Filter
+
+//Scroll to Top
+var link = document.getElementById("back-to-top");
+    var amountScrolled = 250;
+    window.addEventListener('scroll', function(e) {
+        if ( window.pageYOffset > amountScrolled ) {
+            link.classList.add('show');
+        } else {
+            link.className = 'back-to-top';
+        }
+    }); 
+    //Scrolls to Top
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        var distance = 0 - window.pageYOffset;
+        var increments = distance/(500/16);
+        function animateScroll() {
+            window.scrollBy(0, increments);
+            if (window.pageYOffset <= document.body.offsetTop) {
+                clearInterval(runAnimation);
+            }
+        };
+        // Loop the animation function
+        var runAnimation = setInterval(animateScroll, 16);
+    });
